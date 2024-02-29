9e038f99aa9fc55b6aaaee0099d99529504d6429
Samantha Tovah <samantha.tovah@gmail.com>
2024-02-29 21:01:41 +0100

    empire-form.html
    Basic functionality for creating new empires

--- src/main/java/io/github/samanthatovah/stellaris/empiremanager/controller/EmpireController.java
+++ src/main/java/io/github/samanthatovah/stellaris/empiremanager/controller/EmpireController.java
@@ -1,26 +1,45 @@
 package io.github.samanthatovah.stellaris.empiremanager.controller;
 
 import io.github.samanthatovah.stellaris.empiremanager.model.Empire;
+import io.github.samanthatovah.stellaris.empiremanager.model.Origin;
 import io.github.samanthatovah.stellaris.empiremanager.repository.EmpireRepository;
+import io.github.samanthatovah.stellaris.empiremanager.repository.OriginRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
 public class EmpireController {
 
-    private final EmpireRepository empireRepository;
+    @Autowired
+    private EmpireRepository empireRepository;
 
-    public EmpireController(EmpireRepository empireRepository) {
-        this.empireRepository = empireRepository;
-    }
+    @Autowired
+    private OriginRepository originRepository;
 
     @GetMapping("/empires")
     public String showEmpireTable(Model model) {
         List<Empire> empires = empireRepository.findAll();
         model.addAttribute("empires", empires);
         return "empire-table";
+    }
+
+    @GetMapping("/create-empire")
+    public String showCreateEmpireForm(Model model) {
+        List<Origin> origins = originRepository.findAll();
+        model.addAttribute("empire", new Empire());
+        model.addAttribute("origins", origins);
+        return "empire-form";
+    }
+
+    @PostMapping("/create-empire")
+    public String createEmpire(@ModelAttribute Empire empire) {
+        empireRepository.save(empire);
+        return "redirect:/empires";
     }
 }
---
+++ src/main/java/io/github/samanthatovah/stellaris/empiremanager/repository/OriginRepository.java
@@ -0,0 +1,10 @@
+package io.github.samanthatovah.stellaris.empiremanager.repository;
+
+import io.github.samanthatovah.stellaris.empiremanager.model.Origin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OriginRepository extends JpaRepository<Origin, Long> {
+
+}
