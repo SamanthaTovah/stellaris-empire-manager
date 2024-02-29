3179167d7f1cb0abfae02bee61f4fb4955977d0f
Samantha Tovah <samantha.tovah@gmail.com>
2024-02-29 21:30:02 +0100

    code cleanup
    use constructor injection rather than field injection

--- src/main/java/io/github/samanthatovah/stellaris/empiremanager/controller/EmpireController.java
+++ src/main/java/io/github/samanthatovah/stellaris/empiremanager/controller/EmpireController.java
@@ -4,7 +4,6 @@ import io.github.samanthatovah.stellaris.empiremanager.model.Empire;
 import io.github.samanthatovah.stellaris.empiremanager.model.Origin;
 import io.github.samanthatovah.stellaris.empiremanager.repository.EmpireRepository;
 import io.github.samanthatovah.stellaris.empiremanager.repository.OriginRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,11 +13,14 @@ import java.util.List;
 @Controller
 public class EmpireController {
 
-    @Autowired
-    private EmpireRepository empireRepository;
+    private final EmpireRepository empireRepository;
 
-    @Autowired
-    private OriginRepository originRepository;
+    private final OriginRepository originRepository;
+
+    public EmpireController(EmpireRepository empireRepository, OriginRepository originRepository) {
+        this.empireRepository = empireRepository;
+        this.originRepository = originRepository;
+    }
 
     @GetMapping("/empires")
     public String showEmpireTable(Model model) {
