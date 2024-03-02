1d9b42c619537497d8f84f8e6adc9e734f2c480d
Samantha Tovah <samantha.tovah@gmail.com>
2024-03-02 16:08:18 +0100

    add authentication

--- src/main/java/io/github/samanthatovah/stellaris/empiremanager/StellarisEmpireManagerApplication.java
+++ src/main/java/io/github/samanthatovah/stellaris/empiremanager/StellarisEmpireManagerApplication.java
@@ -1,10 +1,12 @@
 package io.github.samanthatovah.stellaris.empiremanager;
 
+import io.github.samanthatovah.stellaris.empiremanager.config.SecurityConfig;
 import io.github.samanthatovah.stellaris.empiremanager.model.Empire;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -14,6 +16,7 @@ import javax.sql.DataSource;
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableJpaRepositories(basePackages = "io.github.samanthatovah.stellaris.empiremanager")
+@Import(SecurityConfig.class)
 public class StellarisEmpireManagerApplication {
 
     public static void main(String[] args) {
