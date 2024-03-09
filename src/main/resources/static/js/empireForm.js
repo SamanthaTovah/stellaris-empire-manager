3da439f4f003c7269a102b6c67a9f155c59438cf
Samantha Tovah <samantha.tovah@gmail.com>
2024-03-09 16:12:14 +0100

    use Select2 for multiselect

---
+++ src/main/resources/static/js/empireForm.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('#traits').select2({
+        placeholder: "Select traits",
+        allowClear: true
+    });
+});
+
+$(document).ready(function() {
+    $('#ethics').select2({
+        placeholder: "Select ethics",
+        allowClear: true
+    });
+});
+
+$(document).ready(function() {
+    $('#civics').select2({
+        placeholder: "Select civics",
+        allowClear: true
+    });
+});
