635cc4ae58623e27300dd28da7d78b86f3d0fd52
Samantha Tovah <samantha.tovah@gmail.com>
2024-03-10 16:26:28 +0100

    add check for docker network already existing

--- database_run.sh
+++ database_run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Create the docker network to be used by both the db container and the empire manager container
-docker network create stellaris-network
+# Check if the stellaris-network already exists
+network_exists=$(docker network ls --filter name=stellaris-network -q)
+if [ -z "$network_exists" ]; then
+  docker network create stellaris-network
+fi
 
 # Run the MySQL container
 docker run --name mysql-db -e MYSQL_ROOT_PASSWORD=mysql -e MYSQL_DATABASE=empiredb --network stellaris-network -p 3306:3306 -d mysql:latest
