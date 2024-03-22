a212e83e40581417873f0fd25142784bb4a975e6
Samantha Tovah <samantha.tovah@gmail.com>
2024-03-22 20:50:30 +0100

    enforce Factory usage for Statistic

--- src/main/java/io/github/samanthatovah/stellaris/empiremanager/domain/statistic/Statistic.java
+++ src/main/java/io/github/samanthatovah/stellaris/empiremanager/domain/statistic/Statistic.java
@@ -1,16 +1,25 @@
 package io.github.samanthatovah.stellaris.empiremanager.domain.statistic;
 
+import lombok.Data;
 import lombok.extern.log4j.Log4j2;
 
 import java.util.Set;
 
 @Log4j2
-public record Statistic(
-        String name,
-        int count,
-        Set<Long> empireIds,
-        int elo
-) implements Comparable<Statistic> {
+@Data
+public class Statistic implements Comparable<Statistic> {
+
+    private final String name;
+    private final int count;
+    private final Set<Long> empireIds;
+    private final int elo;
+
+    Statistic(String name, int count, Set<Long> empireIds, int elo) {
+        this.name = name;
+        this.count = count;
+        this.empireIds = empireIds;
+        this.elo = elo;
+    }
 
     @Override
     public int compareTo(Statistic statistic) {
