28020a52babc50f6d894e0d9959e461b82969617
Samantha Tovah <samantha.tovah@gmail.com>
2024-03-25 15:57:29 +0100

    refactor stats page js to own file

--- src/main/resources/static/js/dataTablesInit.js
+++ src/main/resources/static/js/dataTablesInit.js
@@ -1,7 +1,14 @@
 // due to dynamic content loading and DataTables initialization
 // the content in this file cannot be imported via the fragment
-
 $(window).on('load', function() {
+    $('.stats-table').each(function() {
+        if (!$.fn.DataTable.isDataTable(this)) {
+            $(this).DataTable({
+                "order": []
+            });
+        }
+    });
+
     $('.view-empires-table').DataTable({
         "order": [[4, "desc"]],
         "pageLength": 100,
