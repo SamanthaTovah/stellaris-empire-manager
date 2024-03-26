3fc0c18a20ef17b80f569155e1b8ecffc52db1ac
Samantha Tovah <samantha.tovah@gmail.com>
2024-03-26 12:30:11 +0100

    clean up frontend comments

--- src/main/resources/static/js/elo.js
+++ src/main/resources/static/js/elo.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     $('.empire').click(function() {
         var empireId = $(this).data('empire-id');
-        // Use the empireId to construct the form's ID dynamically and submit
         $('#form-empire-' + empireId).submit();
     });
 });
