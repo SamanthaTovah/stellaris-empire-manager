ff67b3e242e99857dddb5b34ea77d63195faf42f
Samantha Tovah <samantha.tovah@gmail.com>
2024-04-05 22:05:56 +0200

    remove debug print

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-echo "Checking for git.properties in the JAR..."
-jar tf /$JAR_FULL_NAME | grep git.properties && echo "git.properties found!" || echo "git.properties NOT found!"
-
 java -jar -Dspring.profiles.active=$SPRING_PROFILES_ACTIVE /$JAR_FULL_NAME
